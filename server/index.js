69e5e8285f771d7ef4f941017968388d0bb90aab
Yuvraj8823 <y.s.raghuwanshi03@gmail.com>
2022-12-18 19:03:05 +0530

    forking

--- server/index.js
+++ server/index.js
@@ -11,7 +11,17 @@ mongoose.connect(DB,{
     console.log('connection successful')
 }).catch(err=>console.log(err))
 
+const mySchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+    }
+})
 
+const model = new mongoose.model('myschema',mySchema)
 
 app.use(express.static(staticPath))
 app.get('/',(req,res)=>{
