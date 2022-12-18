9e4de8d0df4d4bfe637e785196d141cc3b22fb84
Yuvraj8823 <y.s.raghuwanshi03@gmail.com>
2022-12-18 19:35:01 +0530

    Commit these changes
    client registration

--- server/index.js
+++ server/index.js
@@ -5,11 +5,14 @@ const staticPath = path.join(__dirname,"../")
 const mongoose = require('mongoose')
 const DB = 'mongodb+srv://hackathon-wemakedevs:hackathon-wemakedevs@cluster0.m1h3lbp.mongodb.net/test'
 const username = 'hackathon-wemakedevs'
+
+app.use(express.json())
 mongoose.connect(DB,{
     dbName:'wemakedevs'
 }).then(()=>{
     console.log('connection successful')
 }).catch(err=>console.log(err))
+
 
 const mySchema = new mongoose.Schema({
     username:{
@@ -18,19 +21,25 @@ const mySchema = new mongoose.Schema({
     },
     password:{
         type:String,
+        required:true
     }
+
 })
 
-const model = new mongoose.model('myschema',mySchema)
+const Model = new mongoose.model('myschema',mySchema)
 
 app.use(express.static(staticPath))
 app.get('/',(req,res)=>{
     res.send('/index.html')
 })
-app.post('/register',(req,res)=>{
-    const {username,email,password,cpassword} = req.body;
+app.post('/register',async (req,res)=>{
+    const {username,password} = req.body;
+    const register = new Model({
+        username:username,
+        password:password
+    })    
+    const signup = await register.save()
 })
-
 
 
 
